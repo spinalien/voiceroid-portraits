3b0017df54834e727ff0ed4fe03db662c977ec39
Spinalien <spina@spinalien.net>
2020-07-23 14:23:28 +0900

    パッケージスクリプトの更新

--- ReleaseScript/PackagingVnext.ps1
+++ ReleaseScript/PackagingVnext.ps1
@@ -2,15 +2,15 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 $ErrorActionPreference = 'stop'
 
-$targets = 'ia_next', 'one_next', 'vflower_next', '弦巻マキ_next', '結月ゆかり_next'
-$targetsEnglish = 'ia_next', 'one_next', 'vflower_next', 'maki_next', 'yukari_next'
+$targets = 'ia_next', 'one_next', 'vflower_next', '弦巻マキ_next', '結月ゆかり_next', '琴葉茜_next', '琴葉葵_next'
+$targetsEnglish = 'ia_next', 'one_next', 'vflower_next', 'maki_next', 'yukari_next', 'akane_next', 'aoi_next'
 $packagePath = './pachage/'
 
 If (test-path $packagePath) {
     Remove-Item -Recurse -Force $packagePath
 }
 
-for($i = 0; $i -lt 8; $i++)
+for($i = 0; $i -lt $targets.Length; $i++)
 {
     $targetPath = $(Join-Path $packagePath $targets[$i])
     New-Item $targetPath -ItemType Directory
